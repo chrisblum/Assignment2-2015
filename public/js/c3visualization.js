f1797c11053bbf3af374a43e294b2fb06c976f43
Chris Blum <cblum@ucsd.edu>
2015-05-01 09:32:44 -0700

    Sasdd

--- public/js/c3visualization.js
+++ public/js/c3visualization.js
@@ -2,18 +2,49 @@
   $.getJSON( '/igMediaCounts')
     .done(function( data ) {
       var yCounts = data.users.map(function(item){
-        return item.counts.media;
+        // if (item.counts.followed_by > 50000) {
+        //   return 50000;
+        // }
+        // else {
+
+        return item.counts.followed_by;
+      // }
+      });
+      var xCounts = data.users.map(function(item){
+        return item.counts.follows;
       });
       
-      yCounts.unshift('Media Count');
+      yCounts.unshift('Follower Count');
+      xCounts.unshift('Follow Count');
 
       var chart = c3.generate({
         bindto: '#chart',
         data: {
           columns: [
-            yCounts 
-          ]
+            yCounts,
+            xCounts
+          ],
+          type: 'area',
+          onmouseover: function () {
+            chart.axis.max(1000000);
+          },
+          onmouseout: function() {
+            chart.axis.max(7500);
+          }
+
         }
       });
+
+
+      setTimeout(function () {
+    chart.axis.max(1000000);
+}, 1000);
+
+
+      setTimeout(function () {
+    chart.axis.max(7500);
+}, 2500);
+
+
     });
 })();
